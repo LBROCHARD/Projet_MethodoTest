c121c6a234c075f293c7028a39d1b91d629cde93
LBROCHARD <73283488+LBROCHARD@users.noreply.github.com>
2024-05-02 16:38:08 +0200

    init

---
+++ src/main/java/com/sip/demo/controller/DemoMVCController.java
@@ -0,0 +1,13 @@
+package com.sip.demo.controller;
+
+import com.sip.demo.entities.Etudiant;
+
+import jakarta.persistence.*;
+
+@Entity 
+
+@Table(name = "products") 
+
+public class DemoMVCController { 
+    
+}
